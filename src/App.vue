566b86990f8dd3932c504ef4a3f703641dee67e5
TommasoPezzola <pezzola123@gmail.com>
2023-05-18 16:04:31 +0200

    Aggiunta immagine

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AppHero from './components/AppHero.vue'
 
 export default {
   components: {
-    HelloWorld
+    HelloWorld,
+    AppHero
   }
 }
 </script>
 
 <template>
-  <HelloWorld src="./components/HelloWorld.vue" />
+  <header>
+    <HelloWorld src="./components/HelloWorld.vue" />
+  </header>
+
+  <main>
+    <AppHero src="./components/AppHero.vue" />
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppHero"
+}
+</script>
+
+<template>
+    <div>
+        <img src="../assets/foto.png">
+    </div>
+</template>
+
+<style>
+img {
+    width: 70%;
+    aspect-ratio: 1/1;
+}
+</style>
